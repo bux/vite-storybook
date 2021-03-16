28567ff4ba1bd530cab6f0cfc8ba0be75aea7c7a
bux <github@jonathandavid.de>
2021-03-16 16:46:49 +0100

    back to start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-    <Card />
+    <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
-import Card from "./components/sample/Card.vue"
-
+import HelloWorld from "./components/HelloWorld.vue"
 export default defineComponent({
     name: "App",
     components: {
-        Card,
+        HelloWorld,
     },
 })
 </script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
